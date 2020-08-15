8905f7bae167d25dd3d8639bcc500d2e41729485
Felipe Trost <felipe.trost@gmail.com>
2020-08-14 22:35:47 -0400

    Changed text

--- src/components/about.js
+++ src/components/about.js
@@ -8,7 +8,7 @@ export default ({reference}) => (
     <section className="about" id="about" ref={reference}>
         <Container>
             <h1 className="section-title">Te ahorraremos dinero haciendo magia negra</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h3 className="section-title">Tramitamos recursos de protección en contra de las Isapres, sin costo alguno para ti.</h3>
         </Container>
     </section>
 )
