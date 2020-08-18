7571e81e1287b194a8fa5ca76ae5c2f56b4f2c99
Felipe Trost <felipe.trost@gmail.com>
2020-08-18 16:24:44 -0400

    Changed text

--- src/components/about.js
+++ src/components/about.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default ({reference}) => (
     <section className="about" id="about" ref={reference}>
         <Container>
-            <h1 className="section-title">Te ahorraremos dinero haciendo magia negra</h1>
+            <h1 className="section-title">¿Que hacemos?</h1>
             <h3 className="section-title">Tramitamos recursos de protección en contra de las Isapres, sin costo alguno para ti.</h3>
         </Container>
     </section>
