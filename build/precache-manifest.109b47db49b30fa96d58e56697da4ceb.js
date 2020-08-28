28dec7b76b97c2dea3bab9af36a95e22a81673ed
Felipe Trost <felipe.trost@gmail.com>
2020-08-27 23:15:09 -0400

    Build 8/27/2020

---
+++ build/precache-manifest.109b47db49b30fa96d58e56697da4ceb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4eb12105f8a45654b75d8168d9b12f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a15e6ad741105f78e9c3",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9be03d95c089d89ca827",
+    "url": "./static/css/main.ccb7c6b3.chunk.css"
+  },
+  {
+    "revision": "a15e6ad741105f78e9c3",
+    "url": "./static/js/2.7b40969b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.7b40969b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9be03d95c089d89ca827",
+    "url": "./static/js/main.d91a3136.chunk.js"
+  },
+  {
+    "revision": "f19b92858a340ff948c9",
+    "url": "./static/js/runtime-main.e33b4977.js"
+  },
+  {
+    "revision": "738adf9ea5fcf7d32e63eb658f028cc0",
+    "url": "./static/media/logo.738adf9e.png"
+  }
+]);
