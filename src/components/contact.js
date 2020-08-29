af0ae759d898199c987987384630991e947b6807
Felipe Trost <felipe.trost@gmail.com>
2020-08-28 23:49:29 -0400

    Fixed form labels

--- src/components/contact.js
+++ src/components/contact.js
@@ -18,42 +18,42 @@ export default ({reference, success, close}) => (
             <Card className="form">
                 <Form action="https://paremoselalzaisapre.cl/mail/send.php" method="POST" encType="multipart/form-data">
                     <Form.Group>
-                        <Form.Label forname="name">Nombre Completo</Form.Label>
+                        <Form.Label htmlFor="name">Nombre Completo</Form.Label>
                         <Form.Control type="text" name="name" id="name" required/>
                     </Form.Group>
 
                     <Form.Group>
-                        <Form.Label forname="identification">Cédula de identidad</Form.Label>
+                        <Form.Label htmlFor="identification">Cédula de identidad</Form.Label>
                         <Form.Control type="text" name="identification" id="identification" required/>
                     </Form.Group>
 
                     <Form.Group>
-                        <Form.Label forname="location">Domicilio y Comuna</Form.Label>
+                        <Form.Label htmlFor="location">Domicilio y Comuna</Form.Label>
                         <Form.Control type="text" name="location" id="location" required/>
                     </Form.Group>
 
                     <Form.Group>
-                        <Form.Label forname="isapre">Isapre</Form.Label>
+                        <Form.Label htmlFor="isapre">Isapre</Form.Label>
                         <Form.Control type="text" name="isapre" id="isapre" required/>
                     </Form.Group>
 
                     <Form.Group>
-                        <Form.Label  forname="phone">Teléfono</Form.Label>
+                        <Form.Label  htmlFor="phone">Teléfono</Form.Label>
                         <Form.Control type="text" name="phone" required/>
                     </Form.Group>
                 
                     <Form.Group>
-                        <Form.Label forname="email">Correo</Form.Label>
+                        <Form.Label htmlFor="email">Correo</Form.Label>
                         <Form.Control type="email" name="email" id="email" required/>
                     </Form.Group>
 
                     <Form.Group>
-                        <Form.Label forname="letter">Carta de adecuación o FUN (escaneado lo más claro posible)</Form.Label>
-                        <Form.File type="file" name="letter" id="letter" required/>
+                        <Form.Label htmlFor="letter">Carta de adecuación o FUN (escaneado lo más claro posible, Formato: jpg. o pdf.)</Form.Label>
+                        <Form.File type="file" name="letter" id="letter" accept=".pdf,.png,.jpg" required/>
                     </Form.Group>
 
                     <Form.Group>
-                        <Form.Label forname="message">Mensaje (opcional)</Form.Label>
+                        <Form.Label htmlFor="message">Mensaje (opcional)</Form.Label>
                         <Form.Control as="textarea" name="message" id="message" rows="3" />
                     </Form.Group>
 
